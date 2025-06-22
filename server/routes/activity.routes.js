62bf8b9051debdc1f6f0a83d91232efb78a95aef
Abhishek <Abhishekraut8697@gmail.com>
2025-06-22 15:33:40 +0530

    added acitity schema validation on routes

--- server/routes/activity.routes.js
+++ server/routes/activity.routes.js
@@ -5,12 +5,14 @@ import {
   deleteActivities,
 } from '../Controllers/Activity.controller.js';
 import { auth } from '../middleware/Auth.middleware.js';
+import { activitySchema } from '../validations/user.schema.js';
+import { validate } from '../middleware/Validate.middleware.js';
 
 const router = express.Router();
 
 // All routes require login
 router.get('/', auth, getActivities);
-router.post('/', auth, addActivity);
+router.post('/', auth,validate(activitySchema), addActivity);
 router.delete('/', auth, deleteActivities);
 
 export default router;
